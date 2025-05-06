747bc76acc8a5f6da627f80fb679aa5ca45613c9
Tibart <tibart@biglebowski.nl>
2025-05-06 22:24:29 +0200

    init setup

---
+++ lua/plugins/nvim-dap-ui.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { 
+      'mfussenegger/nvim-dap', 
+      'nvim-neotest/nvim-nio',
+      'theHamsta/nvim-dap-virtual-text'
+    },
+    event = "VeryLazy",
+    config = function()
+
+		  local dap, dapui = require('dap'), require('dapui')
+  		dapui.setup({
+        -- Customize UI lauout
+        layout = {{ 
+          elements = {
+            { id = "breakpoint", size = 0.50 }
+          }
+        }},
+      })
+
+      require("nvim-dap-virtual-text").setup()
+      require("mason-nvim-dap").setup()
+
+      -- Auto open/close UI
+      dap.listeners.after.event_initialized.dapui_config = function()
+  		  dapui.open()
+  		end
+	  	dap.listeners.before.attach.dapui_config = function()
+  		  dapui.open()
+  		end
+  		dap.listeners.before.launch.dapui_config = function()
+  		  dapui.open()
+  		end
+  		dap.listeners.before.event_terminated.dapui_config = function()
+  		  dapui.close()
+  		end
+  		dap.listeners.before.event_exited.dapui_config = function()
+  		  dapui.close()
+  		end
+
+      vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg='#ff0000' })
+      vim.fn.sign_define('DapBreakpoint', { text='' , texthl='DapBreakpoint' })
+
+      vim.api.nvim_set_hl(0, 'DapStopped', { fg='#ffff00' })
+      vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped' })
+      
+      vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle DAP UI" })
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
+      vim.keymap.set("n", "<leader>ds", dap.step_over, { desc = "Step Over" })
+      vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+      vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Step Out" })
+
+  	end
+  },
+}
