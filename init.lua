b4088693ba8af78dbbcdf7b3fd02a9c9d3578de9
Tibart <tibart@biglebowski.nl>
2025-05-21 22:53:12 +0200

    No log

--- init.lua
+++ init.lua
@@ -13,10 +13,10 @@ end
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    local msg = string.format("Welcome back %s!", get_username():gsub("^%l", string.upper))
-    --vim.notify(msg, vim.log.levels.INFO)
+    local username = os.getenv("USER") or os.getenv("USERNAME") or "User"
+    local msg = string.format("Welcome back %s!", username:gsub("^%a", string.upper))
     vim.defer_fn(function ()
-      vim.api.nvim_echo({{msg, 'error'}}, true, {})
-    end, 100)
+      vim.api.nvim_echo({{msg, 'error'}}, false, {})
+    end, 10)
   end,
 })
