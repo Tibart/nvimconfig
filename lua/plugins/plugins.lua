812b0221491bc3999a8888eda6a30ba57a365e01
Tibart <tibart@biglebowski.nl>
2025-05-30 09:12:10 +0200

    transparent

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -3,4 +3,6 @@ return {
   { "norcalli/nvim-colorizer.lua", config = function ()
     require("colorizer").setup()
   end },
+  { "numToStr/Comment.nvim", opts = { } },
+  { "xiyaowong/transparent.nvim" },
 }
